9f2b5bd249faebc2eed55b571f7132dc4eede996
Заманов Надир Расим <Zama_uq57@itstep.edu.az>
2019-02-14 21:09:12 +0400

    Add project files.

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+
+    class Task
+    {
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Comment { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
